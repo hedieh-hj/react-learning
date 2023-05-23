fc9b313b56badc641d9caaf49a299acb17dd8647
hedieh-hj <hediyejamili79@gmail.com>
2023-05-23 21:23:26 +0430

    class - parent.child

---
+++ test3/main.js
@@ -0,0 +1,34 @@
+class Rectangle {
+    constructor(width, height) { //baray sakhtan vorodihay class 
+        this.width = width // ina dar ekhtiar kol class hastn ba in kar
+        this.height = height
+    }
+
+    render() { //sakhatn yek func jadid - faghat name benevisim
+        console.log(this.width, this.height);
+    }
+}
+
+
+
+// var obj = new Rectangle(10, 20);
+
+// obj.render(); //== console.log(obj.height, obj.width);
+
+
+class ColoredRectangle extends Rectangle { //child  + extends + parent 
+    constructor(width, height, color) { // input ha chie ? hame ro mikhaym hata parent 
+        super(width, height) //ers bari az class parent 
+        this.color = color
+    }
+
+    show() {
+        return "<div style='width:" + this.width + "px; height:" + this.height + "px; background:" + this.color + "'></div>"
+    }
+}
+
+var object = new ColoredRectangle(200, 100, "purple");
+document.getElementById("mydiv").innerHTML = object.show();
+
+var object2 = new ColoredRectangle(400, 300, "blue");
+document.getElementById("mydiv2").innerHTML = object2.show();
